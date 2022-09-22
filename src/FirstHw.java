d9d4fb826b2cce87fde07c6d9a60c69cc080acc9
Obzdag <osman.bozdag@yahoo.com>
2022-09-22 22:22:07 +0200

    Hw0 BITTI

--- src/FirstHw.java
+++ src/FirstHw.java
@@ -1,5 +1,9 @@
 public class FirstHw {
     public static void main(String[] args) {
         //TODO: Lütfen ekrana Merhaba Dünya! yazan bir java programı yazınız.
+        System.out.println("Merhaba Dunya");
+
     }
+
+
 }
